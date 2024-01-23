fda856aebdda536673c7f4b4fd346a3e342eb5a9
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2024-01-22 20:48:53 -0800

    Time: 71 ms (88.77%), Space: 16.6 MB (75.18%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:        
+        return self.dfs(arr, 0, "")
+    
+    def dfs(self, arr: List[str], pos: int, res: str) -> int:        
+        # Use a set to check res for duplicate characters
+        if len(res) != len(set(res)):
+            return 0
+        
+        best = len(res)
+        for i in range(pos, len(arr)):
+            best = max(best, self.dfs(arr, i + 1, res + arr[i]))
+        return best
