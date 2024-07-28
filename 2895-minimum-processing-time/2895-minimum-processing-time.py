0a93570d34fae18ae3066b7318db66bd1c92fa93
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2024-07-27 20:15:27 -0700

    Time: 689 ms (6.58%), Space: 33 MB (35.34%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.py
+++ 2895-minimum-processing-time/2895-minimum-processing-time.py
@@ -2,14 +2,14 @@ class Solution:
     def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
         processorTime.sort()
         tasks.sort(reverse=True)
-        l = 0
+        task = 0
         ans = 0
         for i in processorTime:
-            cur=0
-            cnt=0
-            while l<len(tasks) and cnt<4:
-                cur=max(cur,i+tasks[l])
-                l+=1
-                cnt+=1
-            ans=max(ans,cur)
+            core = 0
+            count = 0
+            while task < len(tasks) and core < 4:
+                count = max(count, i + tasks[task])
+                core+=1
+                task+=1
+            ans = max(ans, count)
         return ans
