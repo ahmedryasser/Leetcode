58906b09d5efcd11c6ee491531c4ccb3dcf3331c
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2024-08-07 23:44:56 -0700

    Time: 35 ms (76.76%), Space: 16.6 MB (18.63%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,44 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        lis = list(s.strip())
+        isDigit = False
+        result = ""
+        
+        if s.strip() == "" or s.strip() == "+" or s.strip() == "-":
+            return 0
+        if lis[0] == '-':
+            result += '-'
+            lis.pop(0)
+        elif lis[0] == '+':
+            result += '+'
+            lis.pop(0)
+        for l in lis:
+            
+            if l.isdigit() == True:
+                if l != "0":isDigit = True
+                result+= l
+            else:
+                if isDigit:
+                    resList = list(result)
+                    while resList[0] == "0":
+                        resList.pop(0)
+                if result == "" or result == "+" or result == "-":
+                    return 0
+                if int(result)> pow(2,31) - 1:
+                    return pow(2,31) - 1
+                if int(result)< pow(-2, 31):
+                    return pow(-2,31)
+                return int(result)
+        if result == "":
+            return 0
+        if int(result)> pow(2,31) - 1:
+            return pow(2,31) - 1
+        if int(result)< pow(-2, 31):
+            return pow(-2,31)
+        if isDigit:
+            resList = list(result)
+            while resList[0] == "0":
+                resList.pop(0)
+        return int(result)
+                
+                
