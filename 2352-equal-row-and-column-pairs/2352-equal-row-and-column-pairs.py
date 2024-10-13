15bb3c8f94bf0680ccae81d7cde7a3b6b7dee61c
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2024-10-12 22:26:49 -0700

    Time: 530 ms (19.51%), Space: 21.5 MB (59.32%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,16 +1,17 @@
+from collections import defaultdict
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         result = 0
-        columns = {}
+        columns =  defaultdict(list)
         
         for row in range(len(grid)):
             for col in range(len(grid)):
-                val = columns.get(col,[])
-                val.append(grid[row][col])
-                columns[col] = val
-        print(columns)
+                columns[col].append(grid[row][col]) 
+                
         cols = list(columns.values())
+        
         for row in grid: 
             for col in cols:
                 if col == row: result+=1
+                    
         return result
