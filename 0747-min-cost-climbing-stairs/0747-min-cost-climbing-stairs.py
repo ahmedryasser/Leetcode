da80f7fd9255b54588282faf11f903ea2180ebca
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2025-04-15 15:49:36 -0700

    Time: 10 ms (6.3%), Space: 20.1 MB (7.09%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,14 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         vals = {}
-        def bfs(index): 
+        def search(index): 
             if index >= len(cost):
                 return 0
-            minimum = vals[index] if index in vals else min(bfs(index+1), bfs(index+2))
+            minimum = vals[index] if index in vals else min(search(index+1), search(index+2))
             vals[index] = minimum
             if index==-1:
                 return  minimum
             else:
                 return minimum + cost[index]
-        return bfs(-1)
+        return search(-1)
             
