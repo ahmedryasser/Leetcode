5a49cba97ba5f6d556c911653465b8e27007d268
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2025-04-20 12:15:24 -0700

    Time: 126 ms (31.51%), Space: 26.8 MB (78.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # profit =0
-        # for indexA in range(len(prices)):
-        #     localProfit = 0
-        #     for indexB in range(indexA+1, len(prices)):   
-        #         currProfit = prices[indexB] - prices[indexA]
-        #         if currProfit>localProfit:
-        #             localProfit = currProfit
-        #     if localProfit>profit:
-        #         profit = localProfit
-        # return profit
-        
-        # start = min(prices)
-        # for i,price in enumerate(prices):
-        #     if price == start:
-        #         return max(prices[i:]) - start
-        
-        min_price = float("inf")
+        min_cost = float('inf')
         max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
-
+        for price in prices:
+            min_cost = min(min_cost, price)
+            max_profit = max(price - min_cost, max_profit)
+        
         return max_profit
