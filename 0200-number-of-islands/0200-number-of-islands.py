3d42c0bece8961eb6fb2586ed515f7e4c5099442
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2025-04-21 17:12:40 -0700

    Time: 459 ms (5.06%), Space: 25.4 MB (27.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        number = 0
+        visited = set()
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        def dfs(coord):
+            stack = [coord]
+            visited.add(coord)
+            while stack:
+                node = stack.pop()
+                x,y = node
+                visited.add(node)
+                print(x,y)
+                for direction in directions:
+                    new_x = x+direction[0]
+                    new_y = y+direction[1]
+                    print("new",new_x,new_y)
+                    if new_x>=0 and new_x<len(grid) and new_y>= 0 and new_y<len(grid[0]) and grid[new_x][new_y] == "1" and (new_x,new_y) not in visited:
+                        stack.append((new_x,new_y))
+                        visited.add((new_x,new_y))
 
-        rows = len(grid)
-        cols = len(grid[0])
-        num_islands = 0
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                val = (x,y)
+                if val not in visited and grid[x][y] == "1":
+                    number+=1
+                    dfs((x,y))
+        return number
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    num_islands += 1
-                    grid[r][c] = 0 # grid is now visited
-                    neighbors = []
-                    neighbors.append((r, c))
-                    while neighbors:
-                        row, col = neighbors.pop(0)
-                        if row - 1 >= 0 and grid[row - 1][col] == "1": # left
-                            neighbors.append((row - 1, col))
-                            grid[row - 1][col] = "0"
-                        if row + 1 < rows and grid[row + 1][col] == "1": # right
-                            neighbors.append((row + 1, col))
-                            grid[row + 1][col] = "0"
-                        if col - 1 >= 0 and grid[row][col - 1] == "1": #above
-                            neighbors.append((row, col - 1))
-                            grid[row][col - 1] = "0"
-                        if col + 1 < cols and grid[row][col + 1] == "1": #below
-                            neighbors.append((row, col + 1))
-                            grid[row][col + 1] = "0"
-        return num_islands
+
