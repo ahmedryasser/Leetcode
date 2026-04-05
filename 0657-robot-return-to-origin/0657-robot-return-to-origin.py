bda29c694f1f261c6e179fdbb5aa38bc35b24b3f
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2026-04-05 16:27:31 -0700

    Time: 15 ms (60.35%), Space: 19.1 MB (92.02%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,26 +1,15 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        xStack = []
-        yStack = []
+        x = 0
+        y = 0
         for move in moves:
-            if move == "L":
-                if  xStack and xStack[-1] == "R":
-                    xStack.pop()
-                else:
-                    xStack.append("L")
-            elif move == "R":
-                if xStack and xStack[-1] == "L":
-                    xStack.pop()
-                else:
-                    xStack.append("R")
-            elif move == "U":
-                if yStack and yStack[-1] == "D":
-                    yStack.pop()
-                else:
-                    yStack.append("U")
+            if move == "U":
+                x+=1
             elif move == "D":
-                if yStack and yStack[-1] == "U":
-                    yStack.pop()
-                else:
-                    yStack.append("D")
-        return not xStack and not yStack
+                x-=1
+            elif move == "L":
+                y-=1
+            else:
+                y+=1
+        return x==0 and y==0
+                
